9ea5b1b1a2cba5be1c6e2e431bc57c05b0f481c3
tajmobile1 <114266082+tajmobile1@users.noreply.github.com>
2023-06-21 23:27:33 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -56,9 +56,9 @@ if bool(environ.get("WEBHOOK", False)):
 
     BOT_PICS = (environ.get('BOT_PICS', "motech")).split()
 
-    FORCE_SUB = environ.get("UPDATE_CHANNEL", "")
+    FORCE_SUB = environ.get("UPDATE_CHANNEL", "technicaltaj")
 
-    SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "MoTechGroup")
+    SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "technicaltajrequestgroup")
 
     START_TXT = environ.get("START_TXT", StartTxT)
 
